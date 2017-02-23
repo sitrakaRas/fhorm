23ec9de74405976542e354e2c656c3e515f005c8
sitraka <sitraka@bocasay.fr>
2017-02-23 08:37:22 +0300

    initiate tojo html

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+	createAdditionnalMenu();
+	$(window).on('resize', function(){
+		createAdditionnalMenu();
+	});
+});
+
+function createAdditionnalMenu() {
+	var windowWidth = $(window).width();
+	$('#search-icon .after-icon').css('width', parseInt($("#top-navbar .container").css("margin-right").replace("px", "")) + 15);
+	if (windowWidth < 768) {		
+		if (!$('#top-menu').hasClass('hasAdditionnalMenu')) {
+			$('#top-menu').addClass('hasAdditionnalMenu');
+			$('#main-menu li').each(function() {
+				$(this).clone().addClass('hidden-sm').addClass('hidden-md').addClass('hidden-lg').appendTo('#top-menu .nav');
+			});
+		}	
+	}
+}
